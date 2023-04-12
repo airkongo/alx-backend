23a959da7181fc42b0e03bbf7fede76a9e50f323
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 12:30:18 +0300

    Update 6-job_processor.js

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,13 +1,12 @@
-import {
-  createQueue,
-} from 'kue';
+import { createQueue } from 'kue';
 
 const queue = createQueue();
 
-const sendNotification = (phoneNumber, message) => {
+function sendNotification(phoneNumber, message) {
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 };
 
-queue.process('push_notification_code', (job) => {
+queue.process('push_notification_code', function(job, done) {
   sendNotification(job.data.phoneNumber, job.data.message);
+  done();
 });
