b81d06e73f8da88d3597bdca0599b659e3e8b874
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 12:29:35 +0300

    Update 5-subscriber.js

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,25 +1,25 @@
-import {
-  createClient,
-} from 'redis';
+import { createClient } from 'redis';
 
-const subscriber = createClient();
+const redisClient = createClient();
 
-(async () => {
-  subscriber.subscribe('holberton school channel');
-
-  subscriber.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
-  });
-
-  subscriber.on('connect', () => {
+//connect to redis server
+redisClient.on('connect', function () {
     console.log('Redis client connected to the server');
-  });
+});
 
-  subscriber.on('message', (channel, message) => {
-    console.log(`${message}`);
-    if (message === 'KILL_SERVER') {
-      subscriber.unsubscribe('holberton school channel');
-      subscriber.quit();
-    }
-  });
-})();
+redisClient.on('error', function (error) {
+    console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+//subscribe to holberton school channel
+redisClient.subscribe('holberton school channel');
+
+//listen for messages on channel and print message when received
+redisClient.on('message', function (channel, message) {
+  console.log(`${message}`);
+  if (message === 'KILL_SERVER') {
+//unsubscribe from channel and cancel server connection
+    redisClient.unsubscribe('holberton school channel');
+    redisClient.end(true);
+  }
+});
