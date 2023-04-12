5168011adee7f9b255b5986c40da6415a9947b94
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 12:29:02 +0300

    Update 4-redis_advanced_op.js

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,32 +1,28 @@
-import redis, {
-  createClient,
-} from 'redis';
+import { createClient, print } from 'redis';
 
-const client = createClient();
+const redisClient = createClient();
 
-(async () => {
-  client.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
-  });
+redisClient.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
 
-  client.on('connect', () => {
-    console.log('Redis client connected to the server');
-  });
-})();
+redisClient.on('error', function(error) {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
 
-const schools = {
-  Portland: 50,
-  Seattle: 80,
-  'New York': 20,
-  Bogota: 20,
-  Cali: 40,
-  Paris: 2,
-};
-for (const [key, value] of Object.entries(schools)) {
-  client.hset('HolbertonSchools', key, value, redis.print);
-}
+//set hash key-value in HolbertonSchools list
+redisClient.hset('HolbertonSchools', 'Portland', '50', print);
+redisClient.hset('HolbertonSchools', 'Seattle', '80', print);
+redisClient.hset('HolbertonSchools', 'New York', '20', print);
+redisClient.hset('HolbertonSchools', 'Bogota', '20', print);
+redisClient.hset('HolbertonSchools', 'Cali', '40', print);
+redisClient.hset('HolbertonSchools', 'Paris', '2', print);
 
-client.hgetall('HolbertonSchools', (err, obj) => {
-  if (err) throw err;
-  console.log(obj);
+// retrieve all elements stored in HolbertonSchools list
+redisClient.hgetall('HolbertonSchools', function (error, result) {
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+  console.log(result);
 });
