8b2d4b181bd7d22a3a435dce811d494a912d6a29
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2023-04-12 12:29:51 +0300

    Update 6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,15 +1,20 @@
-import {
-  createQueue,
-} from 'kue';
+import { createQueue } from 'kue';
 
 const queue = createQueue();
 
-const job = queue.create('push_notification_code', {
-  phoneNumber: '4153518780',
-  message: 'This is the code to verify your account',
-}).save((err) => {
-  if (!err) console.log(`Notification job created: ${job.id}`);
+const notification = {
+  'phoneNumber': '4153518780',
+  'message': 'This is the code to verify your account'
+}
+
+const job = queue.create('push_notification_code', notification).save(function (error) {
+  if (!error) {
+    console.log(`Notification job created: ${job.id}`);
+  }
 });
 
-job.on('complete', () => console.log('Notification job completed'));
-job.on('failed', () => console.log('Notification job failed'));
+job.on('complete', function() {
+    console.log('Notification job completed');
+}).on('failed', function() {
+    console.log('Notification job failed');
+});
